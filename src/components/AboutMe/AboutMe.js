78773af359046eae29e0ec3818d242195f0e1720
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2023-07-27 22:30:29 +0300

    Сделана фавиконка, сдача на 2ю итерацию

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -15,7 +15,7 @@ function AboutMe() {
           <p className="aboutMe__text">Таким образом реализация намеченных плановых заданий позволяет оценить значение соответствующий условий активизации. Таким образом сложившаяся структура организации позволяет выполнять важные задания по разработке позиций, занимаемых участниками в отношении поставленных задач.</p>
           <a className="aboutMe__link" href="https://github.com/NataBr1" target="_blank" rel="noreferrer">Github</a>
         </div>
-        
+
         <div>
           <img className="aboutMe__photo" src={photo} alt="Фотография студента"/>
         </div>
